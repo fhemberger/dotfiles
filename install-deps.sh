88ac66e9470913afedf408bc3ac820129b816094
Frederic Hemberger <mail@frederic-hemberger.de>
2016-02-29 05:35:08 +0100

    Update setup scripts

--- install-deps.sh
+++ install-deps.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 
-# Install Homebrew if needed
-if [ ! -f /usr/local/bin/brew ]; then
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-fi
+sudo -v
 
-# Make sure we’re using the latest Homebrew and installed
-brew update && brew upgrade
+# Set rights so we don't need to use sudo for npm or homebrew
+sudo mkdir -p /usr/local
+sudo chown -R $USER /usr/local
 
-# Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
+setup/homebrew
+setup/zsh-setup
 
-# Install wget with IRI support
-brew install wget --enable-iri
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-# Install everything else
-brew install git
-brew install rename
-brew install tree
-
-# Remove outdated versions from the cellar
-brew cleanup
+# Copy dotfiles to ~
+echo "Copying dotfiles ..."
+cp -nr .dotfiles ~
+cp -n .colordiffrc .gitconfig .gitignore_global .inputrc ~
+touch ~/.extra
