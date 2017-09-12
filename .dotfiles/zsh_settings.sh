11f4cd842ae8b312a930b13f850126c1f0fe356d
Frederic Hemberger <mail@frederic-hemberger.de>
2017-09-12 13:04:18 +0200

    Update .dotfiles

--- .dotfiles/zsh_settings.sh
+++ .dotfiles/zsh_settings.sh
@@ -2,8 +2,13 @@
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '^E' edit-command-line
+
 # Insert the last word from the previous line.
 bindkey "^P" insert-last-word
+
+# Enable autocompletion
+autoload -Uz compinit
+compinit
 
 # Automatically quote meta-characters in URLs!
 autoload -U url-quote-magic
@@ -12,6 +17,9 @@ zle -N self-insert url-quote-magic
 # Include dotfiles in globbing
 # https://coderwall.com/p/rfwjlq
 setopt globdots
+
+# Don't share history between sessions/panes
+setopt no_share_history
 
 # Sourcing all the things ...
 for file in ~/.dotfiles/.*; do [ -f "$file" ] && source "$file"; done
