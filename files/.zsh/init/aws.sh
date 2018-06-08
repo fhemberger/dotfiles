b82c5e81f9a608796ccf001306678cf5e5cf8ec2
Frederic Hemberger <mail@frederic-hemberger.de>
2018-06-08 09:22:16 +0200

    aws: Skip brew prefix

--- files/.zsh/init/aws.sh
+++ files/.zsh/init/aws.sh
@@ -3,7 +3,7 @@
 if [ $commands[aws] ]; then
   aws() {
     unfunction "$0"
-    source $(brew --prefix awscli)/libexec/bin/aws_zsh_completer.sh
+    source /usr/local/opt/awscli/libexec/bin/aws_zsh_completer.sh
     $0 "$@"
   }
 fi
