b63aa5b53c82e33602d84e7c5569fbd8bf8b9a1b
Frederic Hemberger <mail@frederic-hemberger.de>
2018-07-11 10:57:45 +0200

    Install 'diff-so-fancy' (for git logs)

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Installing diff-so-fancy ..."
+curl -sSo /usr/local/bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x /usr/local/bin/diff-so-fancy
+
 # Copy dotfiles to ~
 echo "Copying dotfiles ..."
 cp -r files/.zsh "$HOME"
