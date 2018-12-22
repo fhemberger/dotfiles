f9887b037ce632f46b06e44bbb488f6cdcd8d046
Frederic Hemberger <mail@frederic-hemberger.de>
2018-12-22 21:22:13 +0100

    Fix 'diff-so-fancy' installation

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Installing diff-so-fancy ..."
-curl -sSo /usr/local/bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
-chmod +x /usr/local/bin/diff-so-fancy
+sudo curl -sSo /usr/local/bin/diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+sudo chmod +x /usr/local/bin/diff-so-fancy
 
 # Copy dotfiles to ~
 echo "Copying dotfiles ..."
