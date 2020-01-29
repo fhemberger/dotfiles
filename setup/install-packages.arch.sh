b5434e96a2ded64b3d083bc6da6aa2466edd445b
Frederic Hemberger <mail@frederic-hemberger.de>
2020-01-29 20:00:15 +0100

    Update linux packages

--- setup/install-packages.arch.sh
+++ setup/install-packages.arch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo pacman -Syu
-sudo pacman -Sy aws-cli colordiff ncdu pkgfile zsh
+sudo pacman -Sy aws-cli colordiff cronie htop ncdu pkgfile zsh
 sudo pkgfile --update
--- setup/install-packages.ubuntu.sh
+++ setup/install-packages.ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install -y colordiff curl ncdu zsh
+sudo apt-get install -y colordiff curl htop ncdu zsh
