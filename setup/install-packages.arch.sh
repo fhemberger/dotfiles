e65387723bb176cb8464b8f2bd86b5a367f582be
Frederic Hemberger <mail@frederic-hemberger.de>
2020-03-10 15:34:13 +0100

    Arch Linux: Add `arch-audit` and enable pacman hook

--- setup/install-packages.arch.sh
+++ setup/install-packages.arch.sh
@@ -2,6 +2,7 @@
 
 sudo pacman -Syu
 sudo pacman -Sy \
+  arch-audit \
   colordiff \
   cronie \
   dnsutils \
@@ -12,3 +13,7 @@ sudo pacman -Sy \
   unp \
   zsh
 sudo pkgfile --update
+
+# Install arch-audit pacman hook, scans installed packages for known vulnerabilities:
+# https://wiki.archlinux.org/index.php/Pacman#Hooks
+sudo curl -sSL -o /usr/share/libalpm/hooks/arch-audit.hook "https://raw.githubusercontent.com/ilpianista/arch-audit/$(arch-audit --version | awk '{ print $2 }')/hooks/arch-audit.hook"
