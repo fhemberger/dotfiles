6033c62f76d60d68098b9d92e36874ea43fdcdbf
Frederic Hemberger <mail@frederic-hemberger.de>
2020-03-18 09:55:23 +0100

    Arch: Use local copy of arch-audit.hook

--- setup/install-packages.arch.sh
+++ setup/install-packages.arch.sh
@@ -22,4 +22,4 @@ sudo pkgfile --update
 
 # Install arch-audit pacman hook, scans installed packages for known vulnerabilities:
 # https://wiki.archlinux.org/index.php/Pacman#Hooks
-sudo curl -sSL -o /usr/share/libalpm/hooks/arch-audit.hook "https://raw.githubusercontent.com/ilpianista/arch-audit/$(arch-audit --version | awk '{ print $2 }')/hooks/arch-audit.hook"
+sudo ln -sf /usr/share/arch-audit/arch-audit.hook /usr/share/libalpm/hooks/arch-audit.hook
