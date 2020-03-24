3b902e6e75b8aaba082c88013e906bb037e7747a
Frederic Hemberger <mail@frederic-hemberger.de>
2020-03-24 12:27:59 +0100

    Internal machines only: Allow passwordless `sudo` operations

--- setup/install-packages.arch.sh
+++ setup/install-packages.arch.sh
@@ -25,3 +25,11 @@ sudo pkgfile --update
 # Install arch-audit pacman hook, scans installed packages for known vulnerabilities:
 # https://wiki.archlinux.org/index.php/Pacman#Hooks
 sudo ln -sf /usr/share/arch-audit/arch-audit.hook /usr/share/libalpm/hooks/arch-audit.hook
+
+# Disable sudo password for current user
+(
+cat <<-EOF
+# User rules for $USER
+$USER ALL=(ALL) NOPASSWD:ALL
+EOF
+) | sudo tee "/etc/sudoers.d/$USER" > /dev/null
--- setup/install-packages.ubuntu.sh
+++ setup/install-packages.ubuntu.sh
@@ -14,3 +14,11 @@ sudo apt-get install -y \
 # Disable Ubuntu motd spam
 # https://eklitzke.org/disabling-ubuntu-motd-spam
 sudo sed -i 's/^ENABLED=.*/ENABLED=0/' /etc/default/motd-news
+
+# Disable sudo password for current user
+(
+cat <<-EOF
+# User rules for $USER
+$USER ALL=(ALL) NOPASSWD:ALL
+EOF
+) | sudo tee "/etc/sudoers.d/$USER" > /dev/null
