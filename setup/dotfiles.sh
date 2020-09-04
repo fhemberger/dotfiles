901d40af8edcb0af08e1c226e7bda685c51d08bf
Frederic Hemberger <mail@frederic-hemberger.de>
2020-09-04 13:55:15 +0200

    Add config for direnv, hadolint, nano and ranger

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 # Copy dotfiles to ~
 echo "Copying dotfiles ..."
 mkdir -p "$HOME/.config"
+cp -r files/.config/{direnv,nano,ranger} "$HOME/.config"
 cp files/.{colordiffrc,gitattributes,gitconfig,gitignore_global,inputrc} "$HOME"
+cp files/.config/hadolint.yaml "$HOME/.config/hadolint.yaml"
 cp -n files/.gitconfig.local "$HOME"
