7497fde253bf62bc790fdd341fab9bca18f00812
Frederic Hemberger <mail@frederic-hemberger.de>
2020-10-02 19:07:15 +0200

    Arch(.Xsessionrc): Add user's bin dir to PATH

--- install-x11-config.sh
+++ install-x11-config.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 mkdir -p "$HOME/.config"
 cp -r files/.config/{alacritty,i3,polybar,rofi} "$HOME/.config"
 cp files/.config/picom.conf "$HOME/.config/picom.conf"
-cp files/.{xinitrc,Xsresources} "$HOME"
+cp files/.{xinitrc,Xresources,Xsessionrc} "$HOME"
 
 xrdb -merge ~/.Xresources
