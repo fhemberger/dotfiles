9d2dc630991b2b818e9007df3c7fc6d6858b9430
Frederic Hemberger <mail@frederic-hemberger.de>
2020-11-03 11:27:58 +0100

    Move Git config to .config/git

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 # Copy dotfiles to ~
 echo -e "\nCopying dotfiles ..."
 mkdir -p "$HOME/.config"
-cp -r files/.config/{direnv,nano,ranger} "$HOME/.config"
-cp files/.{colordiffrc,gitattributes,gitconfig,gitignore_global,inputrc} "$HOME"
+cp -r files/.config/{direnv,git,nano,ranger} "$HOME/.config"
+cp files/.{colordiffrc,inputrc} "$HOME"
 cp files/.config/hadolint.yaml "$HOME/.config/hadolint.yaml"
-cp -n files/.gitconfig.local "$HOME"
+
+echo "Add your Git user name and email in ~/.config/git/user"
