69ab99a2eb207d1846714577a1ca6e45bc66f12c
Frederic Hemberger <mail@frederic-hemberger.de>
2020-11-26 16:27:59 +0100

    Fish: Clean up path config (macOS)

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -14,7 +14,6 @@ end
 # Make sure to have installed coreutils using `brew install coreutils`.
 # Otherwise use `alias ls="ls -lAhGO"` on macOS for the same results.
 alias ls="ls -lAh --color=auto --group-directories-first"
-alias lsd='ls | lolcat'
 
 # -- Keybindings --------------------------------------------------------------
 set -gx EDITOR nano
@@ -41,21 +40,19 @@ end
 # -- macOS: Paths -------------------------------------------------------------
 if test (uname) = "Darwin"
   set HOMEBREW_NO_ANALYTICS 1
-  set GOBIN $HOME/go/bin
 
-  set    PATH /usr/local/opt/openssl/bin
-  set -a PATH /usr/local/opt/grep/libexec/gnubin
-  set -a PATH /usr/local/opt/coreutils/libexec/gnubin
-  set -a PATH /usr/local/opt/go/libexec/bin
-  set -a PATH $GOBIN
-  set -a PATH /usr/local/opt/python@3.8/bin
-  set -a PATH $HOME/Library/Python/3.8/bin
-  set -a PATH /usr/local/bin
-  set -a PATH /usr/local/sbin
-  set -a PATH /usr/bin
-  set -a PATH /usr/sbin
-  set -a PATH /bin
-  set -a PATH /sbin
+  set -p PATH /usr/local/opt/coreutils/libexec/gnubin
+  set -p PATH /usr/local/opt/findutils/libexec/gnubin
+  set -p PATH /usr/local/opt/grep/libexec/gnubin
+  set -p PATH /usr/local/opt/openssl/bin
 
-  launchctl setenv PATH $PATH
+  if type -q "go"
+    set GOBIN $HOME/go/bin
+
+    set -p PATH /usr/local/opt/go/libexec/bin
+    set -p PATH $GOBIN
+  end
+
+
+  launchctl setenv PATH "$PATH"
 end
