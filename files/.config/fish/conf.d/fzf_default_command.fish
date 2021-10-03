d5b9963b53bbe927c259ff55cc72bd4d42c28056
Frederic Hemberger <mail@frederic-hemberger.de>
2021-10-03 19:10:11 +0200

    fzf: Use `fd` command to find files (fish)

--- files/.config/fish/conf.d/fzf_default_command.fish
+++ files/.config/fish/conf.d/fzf_default_command.fish
@@ -1,1 +1,7 @@
-set -q FZF_DEFAULT_COMMAND "find . -type f -not -path '*/\.git/*'"
+if type -q "fd"
+  set -q FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
+  set -q FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+else
+  set -q FZF_DEFAULT_COMMAND "find . -type f -not -path '*/\.git/*'"
+  set -q FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+end
