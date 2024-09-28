01d9779dcdc1c5687e460b28e7a7ee1b77a6f850
Frederic Hemberger <mail@frederic-hemberger.de>
2024-09-28 14:50:37 +0200

    feat: Remove config files for Linux desktop environments
    It has been quite a while since I last used Linux as desktop environment.
    Configs/tooling are probably horribly outdated by now, better remove them
    entirely. They can always be restored if needed.

--- scripts/generate-packagelist.arch.sh
+++ scripts/generate-packagelist.arch.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pacman -Qqe | grep -v "$(cat "$SCRIPT_DIR/../packages/Pacfile.x11")" | grep -v "$(pacman -Qqm)" > "$SCRIPT_DIR/Pacfile"
-pacman -Qqm | grep -v "$(cat "$SCRIPT_DIR/../packages/Aurfile.x11")" grep -v "paru-bin" > "$SCRIPT_DIR/Aurfile"
+pacman -Qqe | grep -v "$(pacman -Qqm)" > "$SCRIPT_DIR/Pacfile"
+pacman -Qqm | grep -v "paru-bin" > "$SCRIPT_DIR/Aurfile"
