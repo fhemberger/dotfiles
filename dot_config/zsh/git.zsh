7976db09106633f8be2b9b7f1fc7d49e3feafdeb
Frederic Hemberger <mail@frederic-hemberger.de>
2025-12-26 09:36:52 +0100

    feat(git): New aliases

--- dot_config/zsh/git.zsh
+++ dot_config/zsh/git.zsh
@@ -3,10 +3,17 @@
 if [ "${commands[git]}" ]; then
   alias g="git"
 
-  gitclonecd() {
+  git-clone-cd() {
     local base="$(basename "$1" .git)"
     git clone $@ && cd "${2:-$base}" || return
   }
 
-  alias gccd=gitclonecd
+  git-delete-merged-branches() {
+    current_branch="$(git branch --show-current)"
+    git checkout --quiet "$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')"
+    git branch --merged | grep -Ev '(^\*|^\+|master|main)' | xargs -n 1 git branch --delete
+    git checkout --quiet "$current_branch"
+  }
+
+  alias gccd=git-clone-cd
 fi
